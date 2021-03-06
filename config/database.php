104166b6a7aad62a2f48ffdddabf6037f857da06
zhangxiangliang <admin@pushme.top>
2021-03-06 14:30:19 +0800

    feat: 增加数据库默认字符键长度

--- config/database.php
+++ config/database.php
@@ -58,9 +58,7 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => 'InnoDB ROW_FORMAT=DYNAMIC',
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            'options' => [],
         ],
 
         'pgsql' => [
@@ -123,7 +121,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
--- src/Support/Providers/AppServiceProvider.php
+++ src/Support/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Support\Providers;
 
+use Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
     }
 }
